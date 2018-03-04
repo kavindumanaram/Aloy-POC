f8c3e6e0438eb229793a47e92eef6a340317cb3f
Kavindu Manaram <kavindu@calcey.com>
2018-03-04 14:56:02 +0530

    set up project

---
+++ server/src/CrudOperationCodeFirdt/CrudOperationCodeFirdt/Models/NHL/Team.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrudOperationCodeFirdt.Models.NHL
+{
+	public class Team
+	{
+	}
+}
---
+++ server/src/CrudOperationCodeFirdt/CrudOperationCodeFirdt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrudOperationCodeFirdt.Startup))]
+namespace CrudOperationCodeFirdt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

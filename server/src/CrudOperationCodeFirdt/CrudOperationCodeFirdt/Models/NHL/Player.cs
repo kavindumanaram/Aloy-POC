f2dcd0b493b29426c53948113d7133eda356e88f
Kavindu Manaram <kavindu@calcey.com>
2018-03-04 15:16:35 +0530

    add 2 new classes

--- server/src/CrudOperationCodeFirdt/CrudOperationCodeFirdt/Models/NHL/Player.cs
+++ server/src/CrudOperationCodeFirdt/CrudOperationCodeFirdt/Models/NHL/Player.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,31 @@ namespace CrudOperationCodeFirdt.Models.NHL
 {
 	public class Player
 	{
+		/// <summary>
+		/// get and set for jersy number.
+		/// </summary>
+		[Key, Column(Order = 0)]
+		public int PlayerId { get; set; }
+
+		/// <summary>
+		/// get and set for first name.
+		/// </summary>
+		[Key, Column(Order = 1)]
+		public string Firstname { get; set; }
+		
+		/// <summary>
+		/// get and set for last name.
+		/// </summary>
+		[Key, Column(Order = 2)]
+		public string Lastname { get; set; }
+
+		/// <summary>
+		/// get and set for position.
+		/// </summary>
+		public string Position { get; set; }
+
+		public string TeamName { get; set; }
+		public Team Team { get; set; }
+
 	}
 }
--- server/src/CrudOperationCodeFirdt/CrudOperationCodeFirdt/Models/NHL/Team.cs
+++ server/src/CrudOperationCodeFirdt/CrudOperationCodeFirdt/Models/NHL/Team.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,26 @@ namespace CrudOperationCodeFirdt.Models.NHL
 {
 	public class Team
 	{
+		/// <summary>
+		/// get and set for team name.
+		/// </summary>
+		[Key]
+		[MaxLength(30)]
+		public string TeamName { get; set; }
+
+		/// <summary>
+		/// get and set for city.
+		/// </summary>
+		public string City { get; set; }
+
+		/// <summary>
+		/// get and set for provice.
+		/// </summary>
+		public string Province { get; set; }
+
+		/// <summary>
+		/// get and set for player.
+		/// </summary>
+		public List<Player> Players { get; set; }
 	}
 }

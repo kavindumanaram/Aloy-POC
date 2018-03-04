956f8ef753e33603408e402cd1d2d3a20ef9ee15
Kavindu Manaram <kavindu@calcey.com>
2018-03-04 23:12:27 +0530

    WIP: dummy data

---
+++ server/src/CrudOperationCodeFirdt/CrudOperationCodeFirdt/Data/DummyData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CrudOperationCodeFirdt.Models.NHL;
+
+namespace CrudOperationCodeFirdt.Data
+{
+	public class DummyData
+	{
+		public static System.Collections.Generic.List<Team> getTeams()
+		{
+	 List<Team> teams = new List<Team>()
+		{
+			new Team()
+			{
+				TeamName = "Canuckers",
+				City = "London",
+				Province = "BC",
+			},
+			new Team()
+			{
+				TeamName = "Spatecus",
+				City = "Colombo",
+				Province = "WP",
+			}
+		};
+			return teams;
+		}
+
+	private List<Player> getPlayers
+	}
+}

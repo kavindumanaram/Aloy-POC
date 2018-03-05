2b82bb966c7028adda44ee5227f6674fd6d5f78c
Kavindu Manaram <kavindu@calcey.com>
2018-03-05 15:55:39 +0530

    add 2 controllers and 1 view

---
+++ server/src/MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string Index()
+        {
+            return "StoreController.Index";
+        }
+
+	    public string Browse(string data)
+	    {
+		    string message = HttpUtility.HtmlEncode("Store.Browse, data =" + data);
+		    return message;
+	    }
+    }
+}
